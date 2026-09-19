aa671bc4919c18d857d69841b61eae7eb61d780d
hnt t 13 <24020659@vnu.edu.vn>
2026-09-19 18:44:33 +0700

    update

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/hnt/code_test/my_bmi160_project/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/hnt/esp/esp-idf/.git")
+set(GIT_DIR "/home/hnt/code_test/my_bmi160_project/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
